3fa74892a4882543a1aa723ac8e5646ec47ef37e
DardanHamdija <95165258+DardanHamdija@users.noreply.github.com>
2022-05-23 17:08:28 +0200

    Add files via upload

---
+++ cartscript.js
@@ -0,0 +1,184 @@
+
+
+function fotot() {
+     var x = location.hash;
+
+     if (x == "#1") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND1/RAY-BAN/PRODUKTI1.png";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$18,055.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "18055";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "18055";
+          document.getElementsByClassName("brand")[0].innerHTML = "RAY-BAN";
+          document.getElementsByClassName("name")[0].innerHTML = "Ray-Ban Model-1";
+     }
+     else if (x == "#2") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND1/RAY-BAN/PRODUKTI2.png";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$12,995.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "12995";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "12995";
+          document.getElementsByClassName("brand")[0].innerHTML = "RAY-BAN";
+          document.getElementsByClassName("name")[0].innerHTML = "Ray-Ban Model-2 ";
+     }
+
+     else if (x == "#3") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND1/RAY-BAN/PRODUKTI3.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$5755,5.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "5755.5";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "5755.5";
+          document.getElementsByClassName("brand")[0].innerHTML = "RAY-BAN";
+          document.getElementsByClassName("name")[0].innerHTML = "Ray-Ban Model-3 ";
+     }
+
+     else if (x == "#4") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND1/RAY-BAN/PRODUKTI4.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$7,995.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "7995";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "7995";
+          document.getElementsByClassName("brand")[0].innerHTML = "RAY-BAN";
+          document.getElementsByClassName("name")[0].innerHTML = " Ray-Ban Model-4";
+     }
+
+     else if (x == "#5") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND2/VERSACE/PRODUKTI1.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$43,110.0";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "43110";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "43110";
+          document.getElementsByClassName("brand")[0].innerHTML = "VERSACE";
+          document.getElementsByClassName("name")[0].innerHTML = "Versace Model-1";
+     }
+
+     else if (x == "#6") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND2/VERSACE/PRODUKTI2.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$93,525.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "93525";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "93525";
+          document.getElementsByClassName("brand")[0].innerHTML = "VERSACE";
+          document.getElementsByClassName("name")[0].innerHTML = "Versace Model-2 ";
+     }
+
+     else if (x == "#7") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND2/VERSACE/PRODUKTI3.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$1,170,000.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "1170000";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "1170000";
+          document.getElementsByClassName("brand")[0].innerHTML = "VERSACE";
+          document.getElementsByClassName("name")[0].innerHTML = "Versace Model-3 ";
+     }
+
+     else if (x == "#8") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND2/VERSACE/PRODUKTI4.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$214,890.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "214890";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "214890";
+          document.getElementsByClassName("brand")[0].innerHTML = "VERSACE";
+          document.getElementsByClassName("name")[0].innerHTML = "Versace Model-4  ";
+     }
+
+
+     else if (x == "#9") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND3/GUCCI/PRODUKTI1.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$23,715.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "23715";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "23715";
+          document.getElementsByClassName("brand")[0].innerHTML = "GUCCI";
+          document.getElementsByClassName("name")[0].innerHTML = "Gucci Model-1 ";
+     }
+
+
+     else if (x == "#10") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND3/GUCCI/PRODUKTI2.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$13,221.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "13221";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "13221";
+          document.getElementsByClassName("brand")[0].innerHTML = "GUCCI";
+          document.getElementsByClassName("name")[0].innerHTML = "Gucci Model-2";
+     }
+
+
+     else if (x == "#11") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND3/GUCCI/PRODUKTI3.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$50,908.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "50908";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "50908";
+          document.getElementsByClassName("brand")[0].innerHTML = "GUCCI";
+          document.getElementsByClassName("name")[0].innerHTML = "Gucci Model-3";
+     }
+
+     else if (x == "#12") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND3/GUCCI/PRODUKTI4.jfif";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$38,024.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "38024";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "38024";
+          document.getElementsByClassName("brand")[0].innerHTML = "GUCCI";
+          document.getElementsByClassName("name")[0].innerHTML = "Gucci Model-4";
+     }
+
+     else if (x == "#13") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND4/PRADA/PRODUKTI1.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$36,486.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "36486";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "36486";
+          document.getElementsByClassName("brand")[0].innerHTML = "PRADA";
+          document.getElementsByClassName("name")[0].innerHTML = "Prada Model-1";
+     }
+
+     else if (x == "#14") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND4/PRADA/PRODUKTI2.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$21,870.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "21870";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "21870";
+          document.getElementsByClassName("brand")[0].innerHTML = "PRADA";
+          document.getElementsByClassName("name")[0].innerHTML = "Prada Model-2";
+     }
+
+     else if (x == "#15") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND4/PRADA/PRODUKTI3.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$53,910.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "53910";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "53910";
+          document.getElementsByClassName("brand")[0].innerHTML = "PRADA";
+          document.getElementsByClassName("name")[0].innerHTML = "Prada Model-3";
+     }
+
+     else if (x == "#16") {
+          document.getElementsByClassName("hiddentext")[0].style.display = "none";
+          document.getElementsByClassName("cartbox")[0].style.visibility = "visible";
+          document.getElementsByClassName("product")[0].src = "images/BRAND4/PRADA/PRODUKTI4.jpg";
+          document.getElementsByClassName("cmimi")[0].innerHTML = "$32,715.00";
+          document.getElementsByClassName("cmimi1")[0].innerHTML = "32715";
+          document.getElementsByClassName("cmimi2")[0].innerHTML = "32715";
+          document.getElementsByClassName("brand")[0].innerHTML = "PRADA";
+          document.getElementsByClassName("name")[0].innerHTML = "Prada Model-4";
+     }
+}
+
